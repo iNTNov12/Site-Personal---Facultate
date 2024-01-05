f289c87b61868f0488aa8c7d302316786091afe7
intnov12 <87260633+iNTNov12@users.noreply.github.com>
2024-01-05 19:06:30 +0200

    Semi-responsive site

---
+++ script/script.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const albumImagini = document.querySelectorAll('.album-foto img');
+    const albumFull = document.querySelector('.album-foto-full');
+    const imagineFull = document.querySelector('.album-foto-full img');
+    const butonInchide = document.querySelector('.album-foto-full span');
+
+    albumImagini.forEach(function (imagine) {
+        imagine.addEventListener('click', function () {
+            const caleImagine = imagine.getAttribute('src');
+            imagineFull.setAttribute('src', caleImagine);
+            albumFull.style.display = 'flex';
+        });
+    });
+
+    butonInchide.addEventListener('click', function () {
+        albumFull.style.display = 'none';
+    });
+});
